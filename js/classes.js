6b17598ca3cb0c56f72058d5d9a5e1e669be4904
bbraga99 <brunuu.henrique@hotmail.com>
2023-03-15 12:18:11 -0300

    Adicionando classes de ataque e metodos de ataque e defesa

--- js/classes.js
+++ js/classes.js
@@ -59,23 +59,77 @@ class BigMonster extends Character {
 }
 
 class Stage {
-    constructor(fighter1, fighter2, fighter1El, fighter2El) {
+    constructor(fighter1, fighter2, fighter1El, fighter2El,logObj) {
         this.fighter1 = fighter1;
         this.fighter2 = fighter2;
         this.fighter1El = fighter1El;
         this.fighter2El = fighter2El;
+        this.log = logObj;
     }
 
     Start() {
         this.update();
-        //TODO evento do botao de atacar
+        
+        this.fighter1El.querySelector('.attackButton').addEventListener('click',()=> this.doAttack(this.fighter1, this.fighter2));
+        this.fighter2El.querySelector('.attackButton').addEventListener('click',()=> this.doAttack(this.fighter2, this.fighter1));
+
     } 
 
     update() {
         //Fighter 1
-        this.fighter1El.querySelector('.name').innerHTML = this.fighter1.name;
+        this.fighter1El.querySelector('.name').innerHTML = `${this.fighter1.name} - ${this.fighter1.life} HP`
+        let f1Pct = (this.fighter1.life / this.fighter1.maxLife) * 100; // pegando a porcentagem de vida do personagem
+        this.fighter1El.querySelector('.bar').style.width = `${f1Pct}%`
 
         //Fighter 2
-        this.fighter2El.querySelector('.name').innerHTML = this.fighter2.name;
+        this.fighter2El.querySelector('.name').innerHTML = `${this.fighter2.name} - ${this.fighter2.life} HP`;
+        let f2Pct = (this.fighter2.life / this.fighter2.maxLife) * 100 ; // pegando a porcentagem de vida do personagem
+        this.fighter2El.querySelector('.bar').style.width = `${f2Pct}%`
     }
+
+    doAttack(attacking, attacked) {
+        if(attacking.life <= 0 || attacked.life <= 0) {
+            this.log.addMessage((`Attacked is death, YOU WIN !!`));
+            return;
+        } 
+
+        let attackFactor = (Math.random() * 2).toFixed(2);
+        let defenseFactor = (Math.random()* 2).toFixed(2);
+
+        let actualAttack = attacking.attack * attackFactor;
+        let actualDefense = attacked.defense * defenseFactor;
+
+        if(actualAttack > actualDefense) {
+            attacked.life -= actualAttack;
+            this.log.addMessage((`${attacking.name} caused ${actualAttack.toFixed(2)} damaged in ${attacked.name} `));
+        } else {
+            this.log.addMessage((`${attacked.name} defended`));
+        }
+
+        this.update(); // para atualizar sempre quando um ataque é desferido
+
+    }
+}
+
+class Log {
+    list =[];
+
+    constructor(listEl) {
+        this.listEl = listEl;
+        this.render();
+    }
+
+    addMessage(msg) {
+        this.list.push(msg);
+        this.render();
+    }
+
+    render() {
+        this.listEl.innerHTML = '';
+
+        for(let i in this.list) {
+            this.listEl.innerHTML += `<li>${this.list[i]}</li>`
+        }
+    }
+
 }
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const log = new Log(document.querySelector('.log'));
+
 const char = new Knight('Bruno');
 const monster = new LittleMonster();
 
@@ -5,7 +7,9 @@ const stage = new Stage(
     char,
     monster,
     document.querySelector('#char'),
-    document.querySelector('#monster')
+    document.querySelector('#monster'),
+    log
 );
 
 stage.Start();
+
